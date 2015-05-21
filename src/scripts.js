5e298c2956521366be3531e5824b91e1981e7249
Anthony Hatfield <UXVD@UXVD.td.teradata.com>
2015-05-21 10:54:20 -0400

    Modal / Load

--- src/scripts.js
+++ src/scripts.js
@@ -1,13 +1,21 @@
-$( ".message" ).hover(
-	function() {
-		$( ".write-reply" ).addClass( "btn-primary" );
-	}, function() {
-		$( ".write-reply" ).removeClass( "btn-primary" );
+$(".message").hover(
+	function(){
+		$(".write-reply").addClass("btn-primary");
+	},function(){
+		$(".write-reply").removeClass("btn-primary");
 	}
 );
 
-$(".write-reply").click(
-	function() {
+$(".write-reply").click(function(){
+		$(".user-reply").css("display","block");
 		$(".user-reply").addClass("active");
+		$(this).remove();
+		$(".send-reply").show();
 	}
 );
+
+$(".send-reply").click(function(){
+		$(".progress-bar").animate({width: "100%"}, 5000, function(){
+			$(".progress").hide();
+		});
+});
